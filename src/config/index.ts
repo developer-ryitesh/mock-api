557a70eff96d8813ba5392197491c3cd02e03797
riteshmyhub <ryitesh94@gmail.com>
2025-09-30 09:40:44 +0530

    fix

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,7 +5,7 @@ dotenv.config();
 const CONFIG = {
     PORT: Number(process.env.PORT) || 3000,
     HAS_PROD: process.env.NODE_ENV === "production",
-    SERVER_URL: "https://api-6nsy.onrender.com",
+    SERVER_URL: "https://api-lytc.onrender.com",
     get DEV_URL() { return `http://localhost:${this.PORT}` },
     get MODE() { return this.HAS_PROD ? "PRODUCTION" : "DEVELOPMENT" },
     IP: Object.values(os.networkInterfaces()).flat().find(net => {
